ec3bd6e03ca1d97f3d94354a6371992001566f4c
Abdel Rahman Shaltoot <arakmmis@gmail.com>
2023-02-23 06:09:17 +0200

    [Add] Switch between light and night mode

--- app/src/main/java/com/forecasty/App.kt
+++ app/src/main/java/com/forecasty/App.kt
@@ -1,15 +1,34 @@
 package com.forecasty
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import java.time.LocalTime
 
 @HiltAndroidApp
-class App: Application() {
+class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG)
             Timber.plant(Timber.DebugTree())
+
+        setDarkMode()
+    }
+
+    private fun setDarkMode() {
+        val now = LocalTime.now()
+
+        val midnight = LocalTime
+            .of(LocalTime.MIDNIGHT.hour, LocalTime.MIDNIGHT.minute)
+
+        val noon = LocalTime
+            .of(LocalTime.NOON.hour, LocalTime.NOON.minute)
+
+        if (now.isAfter(midnight) && now.isBefore(noon))
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        else
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
 }
